237e9a66fdfd9a7afe7a2bc4b0650bdb3e669da1
JayEom <djawjdgus93@naver.com>
2020-10-31 22:50:47 +0900

    how to use properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food(props) {
+  return <h1>I Like {props.fav}</h1>
+}
 
 function App() {
   return (
     <div className="App">
       <h1>hello !!!</h1>
-      <Potato />
+      <Food fav="김치" />
+      <Food fav="뼈해장국" />
+      <Food fav="삼겹살" />
+      <Food fav="라면" />
+
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Potato() {
-    return (
-        <h6>This is Potato.</h6>   
-    );
-}
-
-export default Potato;
